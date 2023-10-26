6d6d736f54bd2f3a705a6d0137bd970684aa272e
Taha Zubeyir TURGUT <tahazubeyir.turgut@student.ucll.be>
2023-10-26 12:17:41 +0200

    project init

---
+++ Examen-practicum/src/main/java/be/ucll/examenpracticum/ExamenPracticumApplication.java
@@ -0,0 +1,13 @@
+package be.ucll.examenpracticum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenPracticumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenPracticumApplication.class, args);
+	}
+
+}
---
+++ Examen-practicum/src/test/java/be/ucll/examenpracticum/ExamenPracticumApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.examenpracticum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenPracticumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
